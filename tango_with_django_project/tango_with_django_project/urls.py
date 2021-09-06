3dc226d4b216901ae9ed80c8deea62b3f6fb7145
Just_Simon <semseminov@gmail.com>
2021-09-06 17:50:47 +0300

    Add redirection to index page after registration

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -13,17 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from registration.backends.simple.views import RegistrationView
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.urls import reverse
 from django.conf import settings
 from django.conf.urls.static import static
 
 from rango import views
 
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user=None):
+        return reverse('index')
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/register/',
+         MyRegistrationView.as_view(),
+         name='registration_register'),
     path('accounts/', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
